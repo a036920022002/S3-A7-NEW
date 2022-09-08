7bc2344bdac804ae44c063a2c6246cdfe578161c
Yuchen <chengcheng0805@gmail.com>
2022-09-09 01:33:47 +0800

    add .sort ()asc

--- router/modules/home.js
+++ router/modules/home.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const restaurantAll = require('../../models/restaurant.js')
 
 router.get('/', (req, res) => {
-  restaurantAll.find().lean()
+  restaurantAll.find().lean().sort({_id:"asc"})
     .then(restaurants =>
       res.render('index', { restaurants: restaurants }))
     .catch(error => console.error(error))
